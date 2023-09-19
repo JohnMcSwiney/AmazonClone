9916d4079b211faa197ef031095b6dd7f69f512d
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2023-09-18 20:29:13 -0600

    Product

--- amazonclone/src/App.js
+++ amazonclone/src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import Header from './components/header/Header';
-import Home from './components/home/Home';
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
 
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     //BEM
     <div className="app">
-     <Header />
-     <Home/>
-     {/*  */}
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      {/*  */}
     </div>
   );
 }
