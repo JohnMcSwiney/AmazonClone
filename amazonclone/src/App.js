400e4dbc9488eb1c6db70fc5e8243ca03be54de0
Johnny Mcswiney <102752692+JohnMcSwiney@users.noreply.github.com>
2023-09-21 21:37:38 -0600

    Trying to get login handler working

--- amazonclone/src/App.js
+++ amazonclone/src/App.js
@@ -2,45 +2,40 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import Login from "./components/login/Login";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux"; // Import the useDispatch function
+import { auth } from './firebase';
+import { setUser } from './actions'; // Import your action creator
 
-
-import { auth } from './firebase'
 import { Routes, Route } from "react-router-dom";
-function App() {
-  useEffect(() => {
-    // will only run once when the app component loads...
 
+import { Provider } from 'react-redux';
+// import store from './redux/store'; // Adjust the import path to your store
+
+function App() {
+  const dispatch = useDispatch(); // Create a dispatch function
+
+  useEffect(() => {
     auth.onAuthStateChanged((authUser) => {
       console.log("THE USER IS >>> ", authUser);
 
       if (authUser) {
-        // the user just logged in / the user was logged in
-
-        dispatch({
-          type: "SET_USER",
-          user: authUser,
-        });
+        // Dispatch the action to set the user
+        dispatch(setUser(authUser));
       } else {
-        // the user is logged out
-        dispatch({
-          type: "SET_USER",
-          user: null,
-        });
+        // Dispatch the action to set the user to null
+        dispatch(setUser(null));
       }
     });
-  }, []);
+  }, [dispatch]); // Include dispatch as a dependency in useEffect
 
   return (
-    //BEM
     <div className="app">
       <Header />
-
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
       </Routes>
-
-      {/*  */}
     </div>
   );
 }
---
+++ amazonclone/src/actions.js
@@ -0,0 +1,10 @@
+// actions.js
+export const SET_USER = 'SET_USER';
+// Define other action types as needed
+// actions.js
+export const setUser = (user) => ({
+  type: SET_USER,
+  user, // Additional data related to the action
+});
+// Define other action creators as needed
+
